02d94ebab730b9efc2acd1b4c477defa9f640750
Gianluca Mauro <gianluca.mauro@sandstonelab.com>
2021-06-24 07:06:59 +0200

    Added example 02_Namespaces

---
+++ c01/02_Namespaces/main.cpp
@@ -0,0 +1,8 @@
+// main.cpp
+#include "namespaces.h"
+
+int main(int argc, char* argv[])
+{
+    mycode::foo();
+    return 0;
+}
---
+++ c01/02_Namespaces/namespaces.h
@@ -0,0 +1,10 @@
+// namespaces.h
+
+#ifndef NAMESPACES_H
+#define NAMESPACES_H
+
+namespace mycode {
+    void foo();
+}
+
+#endif
