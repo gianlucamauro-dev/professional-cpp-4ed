acacfae1011a79dc43866a9084f156a23e0800bd
Gianluca Mauro <gianluca.mauro@sandstonelab.com>
2021-06-24 06:48:18 +0200

    Added example 01_HelloWorld

---
+++ c01/01_HelloWorld/helloworld2.cpp
@@ -0,0 +1,9 @@
+// helloworld2.cpp
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Program name: " << argv[0] << std::endl;
+    return 0;
+}
